e0be2c3394ba9f41ded4c1a210d6701072737e71
suhaswarrior <64247982+suhaswarrior@users.noreply.github.com>
2023-05-25 22:52:19 +0530

    TableWindow.java

--- Lab/Singelton/BrowserTab.java
+++ Lab/Singelton/BrowserTab.java
@@ -1,11 +1,22 @@
-public class BrowserTab {
- private BrowserHistory history;
-
- public BrowserTab() {
-     this.history = BrowserHistory.getInstance();
- }
-
- public void visitURL(String url) {
-     history.addURL(url);
- }
+import java.util.ArrayList;
+public class TabWindow {
+	public static TabWindow sc;
+	ArrayList<String> urls;
+	private TabWindow() {
+		// TODO Auto-generated constructor stub
+		urls=new ArrayList<String>();
+	}	
+	public static TabWindow getInstance() {
+		if(sc==null)
+			sc=new TabWindow();
+		return sc;
+	}	
+	public void addUrl(String url) {
+		urls.add(url);
+	}	
+	public void showUrls() {
+		for(String u:urls) {
+			System.out.println(u);
+		}
+	}	
 }
